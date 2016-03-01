c006c2f5e199a211d9dd38187de86f6e33a078d8
Julia Kudryavtseva <juliet.kudryavtseva@gmail.com>
2016-03-01 19:33:32 +0200

    Get started

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ticTacToeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ticTacToeApp
+ */
+
+var app = angular.module('ticTacToeApp');
+
+app.controller('MainCtrl', ['$scope', function ($scope) {
+
+}]);
