7573f0b6b3752ff966ea475024fc415dc4a9aa86
Julia Kudryavtseva <juliet.kudryavtseva@gmail.com>
2016-03-07 15:32:09 +0200

    Simple X/O logic

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,5 +11,31 @@
 var app = angular.module('ticTacToeApp');
 
 app.controller('MainCtrl', ['$scope', function ($scope) {
+    $scope.turnNumber = 1;
+    $scope.currentPlayer = 'X'; // 'O'
+    $scope.board = [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+    ];
+// $scope.board[r][c] = 'X'
+    $scope.update = function (index, row) {
+        if (row[index] !== null) {
+            return;
+        }
+        if ($scope.turnNumber % 2 != 0) {
+            $scope.currentPlayer = 'O'
+            row[index] = 'X';
+        } else {
+            $scope.currentPlayer = 'X';
+            row[index] = 'O';
+        }
+        $scope.turnNumber++;
+    };
+
+    $scope.isWin = function() {
+        return null;
+    };
+
 
 }]);
