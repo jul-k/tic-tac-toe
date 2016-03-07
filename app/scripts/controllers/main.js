6962a0d62eeed6e7035d99b8fc3b4fec237567eb
Julia Kudryavtseva <juliet.kudryavtseva@gmail.com>
2016-03-07 16:40:11 +0200

    Write win conditions

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,13 +12,13 @@ var app = angular.module('ticTacToeApp');
 
 app.controller('MainCtrl', ['$scope', function ($scope) {
     $scope.turnNumber = 1;
-    $scope.currentPlayer = 'X'; // 'O'
+    $scope.currentPlayer = 'X';
     $scope.board = [
         [null, null, null],
         [null, null, null],
         [null, null, null]
     ];
-// $scope.board[r][c] = 'X'
+
     $scope.update = function (index, row) {
         if (row[index] !== null) {
             return;
@@ -34,7 +34,55 @@ app.controller('MainCtrl', ['$scope', function ($scope) {
     };
 
     $scope.isWin = function() {
-        return null;
+
+        function verifyArray(array) {
+            var vals = _.uniq(array);
+            if (vals.length != 1) {
+                return false;
+            }
+            return vals[0] != null;
+        }
+        // row
+        var ok = _.map($scope.board, verifyArray); // -> [bool]
+
+        if (_.any(ok)){
+            return true;
+        }
+
+        // column
+        var ok = _.map(_.unzip($scope.board), verifyArray);
+        if (_.any(ok)){
+            return true;
+        }
+
+        // forward diagonal
+
+        var diagonal = [];
+        for (var i = 0; i < $scope.board.length; i++) {
+            for (var j = 0; j < $scope.board[i].length; j++) {
+                if (i == j) {
+                 diagonal.push($scope.board[i][j]);
+                }
+            }
+        }
+        if (verifyArray(diagonal)){
+            return true;
+        }
+
+        // backward diagonal
+
+        var diagonal = [];
+        for (var i = 0; i < $scope.board.length; i++) {
+            for (var j = 0; j < $scope.board[i].length; j++) {
+                if ((i + j) === $scope.board[i].length - 1) {
+                 diagonal.push($scope.board[i][j]);
+                }
+            }
+        }
+        if (verifyArray(diagonal)){
+            return true;
+        }
+        return false;
     };
 
 
